8e2ffce03a79d31b4178d36d20033b3a1b362a44
Lenilson Soares <lenilsonp40@gmail.com>
2025-06-16 20:57:26 -0300

    client

--- CRUD_Hibernate/src/main/java/org/example/Main.java
+++ CRUD_Hibernate/src/main/java/org/example/Main.java
@@ -1,12 +1,8 @@
 package org.example;
 
-import org.example.model.Cliente;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
-import java.util.List;
 
 public class Main {
     private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("crudPU");
